0676d7b0c50e85c6338664ff8418ab16eb0fbf1f
Lucian Cibu <luciancibu@yahoo.com>
2025-12-19 03:03:56 +0200

    Delete original folder with .tf files

--- terraform/provider.tf
+++
@@ -1,7 +0,0 @@
-# Documentation References:
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/account_region
-
-provider "aws" {
-  region  = var.region
-  profile = "default"
-}
